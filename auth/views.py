ed42836baca66f3f2b6f4ab276f36333f36d5854
paisoncodes <iamkomolafe.o.s@gmail.com>
2021-12-07 08:08:46 +0100

    url paths added

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,7 @@
-from django.http.response import Http404
+from django.http.response import Http404, JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 from .serializers import (
     BaseUserSerializer,
     ServiceProviderSerializer
@@ -40,7 +41,7 @@ class CustomerRetrieveUpdateDelete(APIView):
     queryset = BaseUserModel.objects.all()
     serializer_class = BaseUserSerializer
 
-    def get(self, id):
+    def get(self, id, request):
 
         customer = BaseUserModel.objects.get(id=id)
         serializer = BaseUserSerializer(customer)
@@ -49,6 +50,35 @@ class CustomerRetrieveUpdateDelete(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
     
+    def put(self, id, request):
+        customer = BaseUserModel.objects.get(id=id)
+        if customer:
+            serializer = BaseUserSerializer(customer, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({
+                "message": "Invalid User ID",
+                "status": status.HTTP_404_NOT_FOUND
+            })
+    
+    def delete(self, id, request):
+        customer = BaseUserModel.objects.get(id=id)
+        if customer:
+            customer.delete()
+            return Response({
+                "message": "User deleted successfully",
+                "status": status.HTTP_204_NO_CONTENT
+            })
+        else:
+            return Response({
+                "message": "Invalid User ID",
+                "status": status.HTTP_404_NOT_FOUND
+            })
 
 
 class ServiceProvider(APIView):
@@ -70,5 +100,47 @@ class ServiceProvider(APIView):
             serializer.save()
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ServiceProviderRetrieveUpdateDelete(APIView):
+    queryset = ServiceProvider.objects.all()
+    serializer_class = ServiceProviderSerializer
+
+    def get(self, id, request):
+
+        customer = ServiceProvider.objects.get(id=id)
+        serializer = ServiceProviderSerializer(customer)
+        if customer:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+    
+    def put(self, id, request):
+        customer = ServiceProvider.objects.get(id=id)
+        if customer:
+            serializer = ServiceProviderSerializer(customer, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({
+                "message": "Invalid User ID",
+                "status": status.HTTP_404_NOT_FOUND
+            })
+    
+    def delete(self, id, request):
+        customer = ServiceProvider.objects.get(id=id)
+        if customer:
+            customer.delete()
+            return Response({
+                "message": "User deleted successfully",
+                "status": status.HTTP_204_NO_CONTENT
+            })
+        else:
+            return Response({
+                "message": "Invalid User ID",
+                "status": status.HTTP_404_NOT_FOUND
+            })
