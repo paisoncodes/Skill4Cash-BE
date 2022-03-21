20e5dfa57e7e53dcf7f5057e9858d291542d1c90
paisoncodes <iamkomolafe.o.s@gmail.com>
2022-03-21 21:07:57 +0100

    reviews and ratings

--- services/serializers.py
+++ services/serializers.py
@@ -13,8 +13,17 @@ from .models import (
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
-        fields = "__all__"
+        fields = ("service_provider", "rating", "review", "customer")
     
+    def create(self, validated_data):
+        rating = Rating.objects.create(
+            service_provider = validated_data["service_provider"],
+            rating = validated_data["rating"],
+            review = validated_data["review"],
+            customer = validated_data["customer"]
+        )
+
+        rating.save()
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
