90b038fbd6db7fb42bfee7b91ab9316f49125443
kuhmasii <olaisaiah54@gmail.com>
2022-04-08 14:53:55 +0100

    Refactor otp functionality code base

--- otp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import OTPVerification 
-
-class OTPSerializers(serializers.ModelSerializer):
-	class Meta:
-		model = OTPVerification
-		exclude = ('otp',)
