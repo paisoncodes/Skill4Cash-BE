cf0d1f8c0113e39d6d9dfa088f0fc86583db7264
Komolafe Olatunji <iamkomolafe.o.s@gmail.com>
2022-05-22 02:02:28 +0100

    Added trailing backslash

--- services/urls.py
+++ services/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("category/", CreateReadCategory.as_view(), name="categories"),
     path("review/", CreateReadReview.as_view()),
     path("reviews/", ReadSPReviews.as_view()),
-    path("schedule", CreateReadSchedule.as_view()),
+    path("schedule/", CreateReadSchedule.as_view()),
     path("schedules/", ReadSPSchedules.as_view()),
     path("schedule/service-provider/", ReadUpdateDeleteSchedule.as_view())
 ]
