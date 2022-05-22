5c2fbd0cb41478da970501f4f1828e5327ba2e3c
Komolafe Olatunji <iamkomolafe.o.s@gmail.com>
2022-05-22 02:10:29 +0100

    Commented the urls

--- services/urls.py
+++ services/urls.py
@@ -11,9 +11,9 @@ from .views import (
 
 urlpatterns = [
     path("category/", CreateReadCategory.as_view(), name="categories"),
-    path("review/", CreateReadReview.as_view()),
-    path("reviews/", ReadSPReviews.as_view()),
-    path("schedule/", CreateReadSchedule.as_view()),
-    path("schedules/", ReadSPSchedules.as_view()),
-    path("schedule/service-provider/", ReadUpdateDeleteSchedule.as_view())
+    # path("review/", CreateReadReview.as_view()),
+    # path("reviews/", ReadSPReviews.as_view()),
+    # path("schedule/", CreateReadSchedule.as_view()),
+    # path("schedules/", ReadSPSchedules.as_view()),
+    # path("schedule/service-provider/", ReadUpdateDeleteSchedule.as_view())
 ]
