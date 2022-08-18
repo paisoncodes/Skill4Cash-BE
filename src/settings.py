0a8886563bd6dbad59967d74748968a6c26a3b50
paisoncodes <iamkomolafe.o.s@gmail.com>
2022-08-18 18:13:07 +0100

    dd

--- src/settings.py
+++ src/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "channels",
     "corsheaders",
-    "rest_framework_jwt",
     "authentication.apps.AuthConfig",
     "services",
     "chat",
